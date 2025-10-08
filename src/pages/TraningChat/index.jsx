891b54cdc4a84645471b7a5515dd18773149b5e4
vxphuc <phuc2652003@gmail.com>
2025-10-08 13:12:49 +0700

    update

--- src/pages/TraningChat/index.jsx
+++ src/pages/TraningChat/index.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import "./style.css";
+import axios from "axios";
 export default function TraningChat() {
     const [data, setData] = useState({
-        name: "",
-        text: "",
+        prompt_Name: "",
+        prompt_Text: "",
         category: "",
+
     });
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,9 +18,12 @@ export default function TraningChat() {
             <h2>Đào tạo ChatGPT</h2>
             <form onSubmit={handleSubmit} className="data-form">
                 <label>Tên Promt</label>
-                <input type="text" value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
+                <input type="text" value={data.prompt_Name} onChange={(e) => setData({...data, prompt_Name: e.target.value})}/>
                 <label>Nội dung Promt</label>
-                <textarea value={data.text} onChange={(e) => setData({...data, text: e.target.value})}/>
+                <textarea value={data.prompt_Text} onChange={(e) => setData({...data, prompt_Text: e.target.value})}/>
+                <label>Danh mục</label>
+                <input type="text" value={data.category} onChange={(e) => setData({...data, category: e.target.value})}/>
+                <button className="RegisterKol" type="submit">Gửi đào tạo</button>
             </form>
         </div>
     );
